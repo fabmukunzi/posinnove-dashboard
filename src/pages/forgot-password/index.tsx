8fd641924df613b7e0d0968b0ac86a3c446e685f
GarrixA <aphrogarrix10@gmail.com>
2024-09-11 14:58:26 +0200

    feat(password): reset password

---
+++ src/pages/forgot-password/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ForgotPassword from "@components/auth/ForgotPassword";
+
+const page = () => {
+	return (
+		<div className=" flex flex-col items-center justify-center h-screen">
+			<ForgotPassword />
+		</div>
+	);
+};
+
+export default page;
