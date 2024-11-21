691b1ba9df91c013f1d33391957dbd6b246cd62c
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2024-11-21 20:48:32 +0200

    added final touches to create project

--- src/components/projects/personalProject.tsx
+++ src/components/projects/personalProject.tsx
@@ -1,11 +1,10 @@
-import { Button, Carousel } from 'antd';
-import React, { useEffect, useState } from 'react';
+import { Button } from 'antd';
 import PersonalCard from './personal-card';
-import { useRouter } from 'next/router';
 import { useGetProjectsQuery } from '@store/actions/projects';
 import HashLoader from 'react-spinners/HashLoader';
 import NewProject from './new-project';
 import useDisclose from '@utils/hooks/useDisclose';
+import { Plus } from 'lucide-react';
 
 const PersonalProjects = () => {
   const { data: projects, isLoading } = useGetProjectsQuery();
@@ -17,12 +16,13 @@ const PersonalProjects = () => {
           <HashLoader color="#091e6a" />
         </div>
       ) : (
-        <div className='w-full'>
-          <Button className='flex justify-end my-4' onClick={toggle}>
-            Create Project
-          </Button>
+        <div className="w-full">
+          <div className="flex justify-end my-4">
+            <Button icon={<Plus size={20} />} onClick={toggle}>Create Project</Button>
+          </div>
+
           <NewProject open={isOpen} close={close} />
-          <div className="grid md:grid-cols-3 gap-3 w-full">
+          <div className="grid md:grid-cols-3 gap-3 w-full my-8">
             {projects?.data?.projects?.map((project: any, index: number) => (
               <PersonalCard key={index} project={project} />
             ))}
