b4ea5b559415f3bda71f0c40ff283ec5e7123a76
Fabrice MUKUNZI <mukunzifabrice77@gmail.com>
2024-12-04 23:36:12 +0200

    [feat]: integrate interests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
+import 'rsuite/dist/rsuite-no-reset.min.css';
 import type { AppProps } from 'next/app';
 import { ConfigProvider, theme } from 'antd';
 import antdTheme from '@utils/config/antdConfig';
 import { Poppins } from 'next/font/google';
 import { Provider } from 'react-redux';
 import { store } from '@store/index';
+import { CustomProvider } from 'rsuite';
 
 const roboto = Poppins({
   weight: '400',
@@ -13,11 +15,13 @@ const roboto = Poppins({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConfigProvider theme={antdTheme}>
+      <CustomProvider>
       <main className={roboto.className}>
         <Provider store={store}>
           <Component {...pageProps} />
         </Provider>
       </main>
+      </CustomProvider>
     </ConfigProvider>
   );
 }
